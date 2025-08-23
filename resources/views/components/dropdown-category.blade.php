32cd05c39322d8b8ab1b0b17402253d6be4f5689
amjadsethar <amjadsethar@gmail.com>
2025-08-24 00:04:44 +0500

    push

---
+++ resources/views/components/dropdown-category.blade.php
@@ -0,0 +1,7 @@
+<select class="form-select" name="{{ $dropdownName }}">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+    <option>D</option>
+    <option>E</option>
+</select>
